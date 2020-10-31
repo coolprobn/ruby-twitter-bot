91c2a3aaa9b29809a6b77988d874a16a36adcbb0
Prabin Poudel <probnpoudel@gmail.com>
2020-10-31 18:05:46 +0545

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.7.2'
+
+gem 'twitter', '~> 7.0.0'
+gem 'figaro', '~> 1.2.0'
+
+group :development, :test do
+  gem 'pry-byebug', '~> 3.9.0'
+end
---
+++ app/services/twitter/re_tweet_service.rb
@@ -0,0 +1,113 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'twitter'
+require 'figaro'
+require 'pry-byebug'
+
+Figaro.application = Figaro::Application.new(
+  environment: 'production',
+  path: File.expand_path('config/application.yml')
+)
+
+Figaro.load
+
+module Twitter
+  class ReTweetService
+    attr_reader :config
+
+    def initialize
+      @config = twitter_api_config
+    end
+
+    def perform
+      while true
+        re_tweet
+      end
+    end
+
+    private
+
+    MAXIMUM_HASHTAG_COUNT = 5
+    HASHTAGS_TO_WATCH = %w[#rails #ruby #Ruby #RubyOnRails #Rails]
+
+    def twitter_api_config
+      {
+        consumer_key: ENV['CONSUMER_KEY'],
+        consumer_secret: ENV['CONSUMER_SECRET'],
+        access_token: ENV['ACCESS_TOKEN'],
+        access_token_secret: ENV['ACCESS_TOKEN_SECRET']
+      }
+    end
+
+    def rest_client
+      Twitter::REST::Client.new(config)
+    end
+
+    def stream_client
+      Twitter::Streaming::Client.new(config)
+    end
+
+    def hashtags(tweet)
+      tweet_hash = tweet.to_h
+      extended_tweet = tweet_hash[:extended_tweet]
+
+      (extended_tweet && extended_tweet[:entities][:hashtags]) || tweet_hash[:entities][:hashtags]
+    end
+
+    def tweet?(tweet)
+      tweet.is_a?(Twitter::Tweet)
+    end
+
+    def english_tweet?(tweet)
+      tweet.lang.eql?('en')
+    end
+
+    def retweet?(tweet)
+      tweet.retweet?
+    end
+
+    def allowed_hashtags?(tweet)
+      includes_allowed_hashtags = false
+
+      hashtags(tweet).each do |hashtag|
+        if HASHTAGS_TO_WATCH.include?(hashtag[:text])
+          includes_allowed_hashtags = true
+
+          break
+        end
+      end
+
+      includes_allowed_hashtags
+    end
+
+    def allowed_hashtag_count?(tweet)
+      hashtags(tweet)&.count <= MAXIMUM_HASHTAG_COUNT
+    end
+
+    def sensitive_tweet?(tweet)
+      tweet.possibly_sensitive?
+    end
+
+    def should_re_tweet?(tweet)
+      tweet?(tweet) && english_tweet?(tweet) && !retweet?(tweet) && allowed_hashtag_count?(tweet) && !sensitive_tweet?(tweet) && allowed_hashtags?(tweet)
+    end
+
+    def re_tweet
+      stream_client.filter(:track => HASHTAGS_TO_WATCH.join(',')) do |tweet|
+        if should_re_tweet?(tweet)
+          byebug
+          rest_client.retweet tweet
+        end
+      end
+    rescue StandardError => e
+      puts "=========Error========\n#{e.message}"
+
+      puts "Waiting for 60 seconds .... \n"
+
+      sleep 60
+    end
+  end
+end
+
+Twitter::ReTweetService.new.perform
